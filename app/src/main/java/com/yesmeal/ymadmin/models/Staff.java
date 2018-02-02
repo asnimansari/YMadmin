02b2826c2680286d8b98cc3b950bde5876ad0c76
Asnim P Ansari <asnim96@gmail.com>
2018-02-03 00:18:00 +0530

    Added Staff and Zone models

---
+++ app/src/main/java/com/yesmeal/ymadmin/models/Staff.java
@@ -0,0 +1,11 @@
+package com.yesmeal.ymadmin.models;
+
+/**
+ * Created by asnimansari on 03/02/18.
+ */
+
+public class Staff {
+    public String  staffName;
+    public String  staffMobile;
+    public String  staffEmail;
+}
---
+++ app/src/main/java/com/yesmeal/ymadmin/models/Zone.java
@@ -0,0 +1,9 @@
+package com.yesmeal.ymadmin.models;
+
+/**
+ * Created by asnimansari on 03/02/18.
+ */
+
+public class Zone {
+    public String zoneName;
+}
