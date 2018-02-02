d2562502c2c1808fe8c36bca69eafac5e6ef9e77
Asnim P Ansari <asnim96@gmail.com>
2018-02-03 01:28:11 +0530

    Added Listneres to each  item

--- app/src/main/java/com/yesmeal/ymadmin/activities/StaffListing.kt
+++ app/src/main/java/com/yesmeal/ymadmin/activities/StaffListing.kt
@@ -4,15 +4,49 @@ import android.content.Intent
 import android.os.Bundle
 import android.support.design.widget.Snackbar
 import android.support.v7.app.AppCompatActivity
+import android.util.Log
+import android.widget.ArrayAdapter
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.ValueEventListener
 import com.yesmeal.ymadmin.R
+import com.yesmeal.ymadmin.models.Shop
+import com.yesmeal.ymadmin.models.Staff
+import com.yesmeal.ymadmin.util.CusUtils
 import kotlinx.android.synthetic.main.activity_staff_listing.*
+import kotlinx.android.synthetic.main.content_main.*
 
 class StaffListing : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_staff_listing)
-//        setSupportActionBar(toolbar)
+
+        var databaseReference = CusUtils.getDatabase().reference.child("staffs")
+        databaseReference.addValueEventListener(object : ValueEventListener {
+            override fun onDataChange(p0: DataSnapshot?) {
+
+
+                var staffs = ArrayList<String>();
+                staffs.clear()
+
+                for (each_shop  in p0!!.children){
+                    var staff: Staff? = each_shop.getValue(Staff::class.java)
+                    Log.e("SHOP_NAME",staff?.staffName)
+                    staffs.add(staff?.staffName!!)
+                }
+
+                var staffAdapter = ArrayAdapter(baseContext,android.R.layout.simple_spinner_item,staffs)
+
+                listView.adapter = staffAdapter
+
+            }
+
+            override fun onCancelled(p0: DatabaseError?) {
+
+            }
+
+        })
 
 
 
